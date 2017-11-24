80a24d0d11cb442cde9a2cd71ce5dae2ba224a4e
Bailey Wickham <baileywickham@gmail.com>
2017-11-24 15:06:06 -0800

    removed bad import statement

--- distanceFromStart.java
+++ distanceFromStart.java
@@ -1,59 +1,9 @@
-import java.lang.Math; // headers MUST be above the first class
-import java.util.*;
-import java.net.*;
-import static com.googlecode.charts4j.Color.*;
-import static com.googlecode.charts4j.UrlUtil.normalize;
-import static org.junit.Assert.assertEquals;
-
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.googlecode.charts4j.AxisLabels;
-import com.googlecode.charts4j.AxisLabelsFactory;
-import com.googlecode.charts4j.AxisStyle;
-import com.googlecode.charts4j.AxisTextAlignment;
-import com.googlecode.charts4j.Color;
-import com.googlecode.charts4j.Data;
-import com.googlecode.charts4j.DataUtil;
-import com.googlecode.charts4j.Fills;
-import com.googlecode.charts4j.GCharts;
-import com.googlecode.charts4j.Line;
-import com.googlecode.charts4j.LineChart;
-import com.googlecode.charts4j.LineStyle;
-import com.googlecode.charts4j.LinearGradientFill;
-import com.googlecode.charts4j.Plots;
-import com.googlecode.charts4j.Shape;
-
 
 public class HelloWorld
 {
   // arguments are passed using the text field below this editor
   public static void main(String[] args)
   {
-
-      Line line1 = Plots.newLine(Data.newData(mywebsite), Color.newColor("CA3D05"), "My Website.com");
-      line1.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
-      line1.addShapeMarkers(Shape.DIAMOND, Color.newColor("CA3D05"), 12);
-      line1.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
-      Line line2 = Plots.newLine(Data.newData(competition), SKYBLUE, "Competition.com");
-      line2.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
-      line2.addShapeMarkers(Shape.DIAMOND, SKYBLUE, 12);
-      line2.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
-
-      final int NUM_POINTS = 10000;
-      LineChart chart = GCharts.newLineChart(line1, line2);
-      chart.setSize(600, 450);
-      chart.setTitle("Distace", WHITE, 14);
-      chart.addHorizontalRangeMarker(40, 60, Color.newColor(RED, 30));
-      chart.addVerticalRangeMarker(70, 90, Color.newColor(GREEN, 30));
-      chart.setGrid(25, 25, 3, 2);
-
-
-
       int r = 0;
     ArrayList<Double> al = new ArrayList<Double>();
     Random rand = new Random();
